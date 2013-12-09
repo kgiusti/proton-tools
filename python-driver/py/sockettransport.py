36503ca30e88a4d3eab57afedac0f83aa641a5df
Kenneth Giusti <kgiusti@redhat.com>
2013-12-09 16:05:58 -0500

    checkpoint

---
+++ python-driver/py/sockettransport.py
@@ -0,0 +1,276 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import socket, errno, time, heapq, select
+from proton import Transport
+
+
+class SocketTransport(object):
+    """Provides network I/O for a Proton Transport via a socket.
+    """
+    def __init__(self, socket, name=None):
+        """socket - Python socket. Expected to be configured and connected.
+        name - optional name for this SocketTransport
+        """
+        self._name = name
+        self._socket = socket
+        self._transport = Transport()
+        self._read_done = False
+        self._write_done = False
+        self._next_tick = 0
+
+    def fileno(self):
+        """Allows use of a SocketTransport by the select module.
+        """
+        return self._socket.fileno()
+
+    @property
+    def transport(self):
+        return self._transport
+
+    @property
+    def socket(self):
+        return self._socket
+
+    @property
+    def name(self):
+        return self._name
+
+    def tick(self, now):
+        """Invoke the transport's tick method.  'now' is seconds since Epoch.
+        Returns the timestamp for the next tick, or zero if no next tick.
+        """
+        self._next_tick = self._transport.tick(now)
+        return self._next_tick
+
+    @property
+    def next_tick(self):
+        """Timestamp for next call to tick()
+        """
+        return self._next_tick
+
+    def need_read(self):
+        """True when the Transport requires data from the network layer.
+        """
+        return (not self._read_done) and self._transport.capacity() > 0
+
+    def need_write(self):
+        """True when the Transport has data to write to the network layer.
+        """
+        return (not self._write_done) and self._transport.pending() > 0
+
+    def read_input(self):
+        """Read from the network layer and processes all data read.  Can
+        support both blocking and non-blocking sockets.
+        """
+        if self._read_done:
+            return None
+
+        c = self._transport.capacity()
+        if c < 0:
+            try:
+                self._socket.shutdown(socket.SHUT_RD)
+            except:
+                pass
+            self._read_done = True
+            return None
+
+        if c > 0:
+            try:
+                buf = self._socket.recv(c)
+                if buf:
+                    self._transport.push(buf)
+                    return len(buf)
+                # else socket closed
+                self._transport.close_tail()
+                self._read_done = True
+                return None
+            except socket.timeout, e:
+                raise  # let the caller handle this
+            except socket.error, e:
+                err = e.args[0]
+                if (err != errno.EAGAIN and
+                    err != errno.EWOULDBLOCK and
+                    err != errno.EINTR):
+                    # otherwise, unrecoverable:
+                    self._transport.close_tail()
+                    self._read_done = True
+                raise
+            except:  # beats me...
+                self._transport.close_tail()
+                self._read_done = True
+                raise
+        return 0
+
+    def write_output(self):
+        """Write data to the network layer.  Can support both blocking and
+        non-blocking sockets.
+        """
+        if self._write_done:
+            return None
+
+        c = self._transport.pending()
+        if c < 0:  # output done
+            try:
+                self._socket.shutdown(socket.SHUT_WR)
+            except:
+                pass
+            self._write_done = True
+            return None
+
+        if c > 0:
+            buf = self._transport.peek(c)
+            try:
+                rc = self._socket.send(buf)
+                if rc > 0:
+                    self._transport.pop(rc)
+                    return rc
+                # else socket closed
+                self._transport.close_head()
+                self._write_done = True
+                return None
+            except socket.timeout, e:
+                raise # let the caller handle this
+            except socket.error, e:
+                err = e.args[0]
+                if (err != errno.EAGAIN and
+                    err != errno.EWOULDBLOCK and
+                    err != errno.EINTR):
+                    # otherwise, unrecoverable
+                    self._transport.close_head()
+                    self._write_done = True
+                raise
+            except:
+                self._transport.close_tail()
+                self._write_done = True
+                raise
+        return 0
+
+    def done(self):
+        return self._write_done and self._read_done
+
+
+class SocketTransports(object):
+    """Container for SocketTransports.  Provides convenience methods for I/O
+    and timer event processing.
+    """
+
+    def __init__(self):
+        self._transports = {}
+        self._timer_heap = [] # (next_tick, sockettransport)
+
+    def add(self, socket_transport):
+        if not socket_transport.name:
+            raise KeyError('SocketTransport not named')
+        if socket_transport.name in self._transports:
+            raise KeyError('SocketTransport name clash')
+        self._transports[socket_transport.name] = socket_transport
+
+    def remove(self, name):
+        del self._transports[name]
+
+    def need_io(self):
+        """Return a pair of lists containing those socket-transports that are
+        read-blocked and write-ready.
+        """
+        readfd = []
+        writefd = []
+        for st in self._transports.itervalues():
+            if st.need_read():
+                readfd.append(st)
+            if st.need_write():
+                writefd.append(st)
+        return (readfd, writefd)
+
+    def get_next_tick(self):
+        """Returns the next pending timeout timestamp in seconds since Epoch,
+        or 0 if no pending timeouts.  process_io() must be called at least
+        once prior to the timeout.
+        """
+        if not self._timer_heap:
+            return 0
+        return self._timer_heap[0][0]
+
+    def process_io(self, readable, writable):
+        """Does all I/O and timer related processing.  readble is a sequence of
+        sockettransports whose sockets are readable.  'writable' is a sequence
+        of sockettransports whose sockets are writable. This method returns a
+        set of all the SocketTransport in the container that have been
+        processed (this includes the arguments plus any that had timers
+        processed).  Use get_next_tick() to determine when process_io() must
+        be invoked again.
+        """
+
+        work_list = set()
+        for st in readable:
+            st.read_input()
+            # check if process_read set a timer
+            if st.next_tick == 0:
+                st.tick(time.time())
+            if st.next_tick:
+                heapq.heappush(self._timer_heap, (st.next_tick, st))
+            work_list.add(st)
+
+        # process any expired transport ticks
+        while self._timer_heap and self._timer_heap[0][0] <= time.time():
+            st = heapq.heappop(self._timer_heap)
+            st.tick(time.time())
+            if st.next_tick:   # timer reset
+                heapq.heappush(self._timer_heap, (st.next_tick, st))
+            work_list.add(st)
+
+        for st in writable:
+            st.write_output()
+            # check if process_write set a timer
+            if st.next_tick == 0:
+                st.tick(time.time())
+            if st.next_tick:
+                heapq.heappush(self._timer_heap, (st.next_tick, st))
+            work_list.add(st)
+
+        return work_list
+
+
+    def do_io(self, deadline=None):
+        """Wait for I/O or timer events on all of the SocketTransports in the
+        container.  deadline - seconds since Epoch, block waiting for I/O and
+        timer events until either I/O or timers expire or this timestamp is
+        reached.  If deadline is None, block indefinately until either I/O
+        occurs or a timer expires.
+        """
+
+        readfd, writefd = self.need_io()
+
+        timeout = None
+        next_tick = self.get_next_tick()
+        if next_tick:
+            deadline = deadline if (deadline and deadline < next_tick) else next_tick
+        if deadline:
+            now = time.time()
+            deadline = 0 if deadline <= now else now - deadline
+
+        readable,writable,ignore = select.select(readfd,writefd,[],timeout)
+
+        return self.process_io(readable, writable)
+
+
+__all__ = [
+    "SocketTransport",
+    "SocketTransports"
+    ]
---
+++ python-driver/tests/socket-client.py
@@ -0,0 +1,470 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import optparse, sys, time, uuid
+import re, socket, select, errno
+import sockettransport
+import proton
+
+"""
+This module implements a simple proton client which directly controls its
+socket resources.
+"""
+
+
+class EndpointEventHandler(object):
+    """State machine interface for generic Proton endpoints
+    """
+    def __init__(self, endpoint):
+        self._endpoint = endpoint
+
+    def uninit(self):
+        """Called when local state is UNINIT."""
+        self._endpoint.open()
+
+    def active(self):
+        """Called while both local and remote states are ACTIVE."""
+        pass  # type specific
+
+    def remoteClosed(self):
+        """Called when local state is ACTIVE and remote state is CLOSED."""
+        self._endpoint.close()
+
+    def closed(self):
+        """Called while both local and remote states are CLOSED."""
+        pass # optional
+
+
+class ReceiverEventHandler(EndpointEventHandler):
+    """Process state changes to a Proton Receive Link."""
+    def __init__(self, link):
+        super(ReceiverEventHandler, self).__init__(link)
+
+    def active(self):
+        pass
+
+    def closed(self):
+        pass
+
+
+class SenderEventHandler(EndpointEventHandler):
+    """Process state changes to a Proton Sender Link."""
+    def __init__(self, link):
+        super(SenderEventHandler, self).__init__(link)
+
+    def active(self):
+        pass
+
+    def closed(self):
+        pass
+
+
+
+class Peer(sockettransport.SocketTransport):
+    """Represents a remote destination for messages send by this client.
+    """
+    def __init__(self, name, socket, count):
+
+        super(Peer, self).__init__(socket, name)
+        self.connection = proton.Connection()
+        self.transport.bind(self.connection)
+        self.sasl = proton.SASL(self.transport)
+        self.sasl.mechanisms("ANONYMOUS")
+        self.sasl.client()
+
+        self.msg_count = count
+        self.msgs_sent = 0
+        self.replys_received = 0
+        #self.next_tick = 0
+
+    # def process_read(self):
+    #     x = self.io_socket.read_input()
+    #     print("read_input() returned %s" % x)
+
+    # def process_write(self):
+    #     x = self.io_socket.write_output()
+    #     print("write_output() returned %s" % x)
+
+    # def fileno(self):
+    #     return self.io_socket.fileno()
+
+    def do_send(self, sender):
+        print("do_send")
+        if self.msgs_sent < self.msg_count:
+            print "sender.credit %s" % sender.credit
+            #if sender.credit == 0:
+            #    print("requesting credit")
+            #    sender.offered(self.msg_count - self.msgs_sent)
+
+            msg = proton.Message()
+            msg.address="amqp://0.0.0.0:5672"
+            msg.subject="Hello World!"
+            msg.body = "First the world, then the galaxy!"
+
+
+            self.msgs_sent += 1
+            delivery = sender.delivery( "%s" % self.msgs_sent )
+            rc = sender.send( msg.encode() )
+            print("rc=%s" % rc)
+            sender.advance()  # indicates we are done writing to delivery
+            delivery.settle()
+
+
+    #                     delivery = pn_delivery(d.sender, "delivery-%d" %
+    #                                            d.msgs_pending)
+    #                     rc = pn_link_send(d.sender, "message")
+    #                     assert(rc >= 0)
+    #                     d.msgs_pending -= 1
+    #                     pn_link_advance(d.sender)
+    #                     pn_delivery_settle(delivery)
+
+
+
+    def do_recv(self, receiver):
+        print("do_recv")
+        if receiver.credit == 0:
+            if self.replys_received < self.msg_count:
+                receiver.flow(1)
+        
+
+
+def main(argv=None):
+
+    _usage = """Usage: %prog [options] [<"Message text">]"""
+    parser = optparse.OptionParser(usage=_usage)
+    parser.add_option("-a", dest="targets", action="append", type="string",
+                      help="<addr>[,<addr>]* \tThe target address(es) [amqp://<domain>[:<port>]]")
+    parser.add_option("-c", dest="msg_count", type="int", default=1,
+                      help="Send <n> messages to each target")
+    parser.add_option("-R", dest="get_replies", action="store_true",
+                      help="Wait for a reply from each message")
+    #parser.add_option("-H", dest="hostname",
+    #                  help="Address to listen on for responses")
+
+    opts, msg_text = parser.parse_args(args=argv)
+    if not msg_text:
+        msg_text = "Hey There!"
+    if opts.targets is None:
+        opts.targets = ["amqp://0.0.0.0:5672"]
+
+    # listener = None
+    # reply_addr = None
+    # hostname = opts.hostname || socket.gethostname()
+    # listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # listener.setblocking(0) # non-blocking
+    # listener.bind((socket.getfqdn(hostname),0))
+    # reply_addr = "amqp://%s:%d" % listener.getsockname()
+    # listener.listen(5)
+
+    peers = sockettransport.SocketTransports()
+
+    regex = re.compile(r"^amqp://([a-zA-Z0-9.]+)(:([\d]+))?$")
+    for t in opts.targets:
+        print("Connecting to %s" % str(t))
+        x = regex.match(t)
+        if not x:
+            raise Exception("Bad address syntax: %s" % str(t))
+        matches = x.groups()
+        host = matches[0]
+        port = int(matches[2]) if matches[2] else None
+        addr = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+        if not addr:
+            print("Could not translate address '%s'" % str(t))
+            continue
+        s = socket.socket(addr[0][0], addr[0][1], addr[0][2])
+        s.setblocking(0) # 0=non-blocking
+        try:
+            s.connect(addr[0][4])
+        except socket.error, e:
+            if e[0] != errno.EINPROGRESS:
+                raise
+
+        # ssn = pn_session(conn)
+        # slink = pn_sender(ssn, "sender")
+        # dst = pn_link_target(slink)
+        # pn_terminus_set_address(dst, "socket-target")
+        # rlink = None
+        # if opts.get_replies:
+        #     rlink = pn_receiver(ssn, "receiver")
+        #     src = pn_link_src(rlink)
+        #     pn_terminus_set_address(src, "socket-source")
+
+        # now open all the engine endpoints
+        #pn_connection_open(conn)
+        #pn_session_open(ssn)
+        #pn_link_open(slink)
+        #pn_link_open(rlink)
+
+        peers.add(Peer(uuid.uuid4().hex, s, opts.msg_count))
+
+    sent = 0
+    replies = 0
+    done = False
+    while not done:
+
+        #
+        # Poll for I/O & timers
+        #
+
+        readfd, writefd = peers.need_io()
+
+        timeout = None
+        deadline = peers.get_next_tick()
+        if deadline:
+            now = time.time()
+            timeout = 0 if deadline <= now else deadline - now
+
+        print("select start (t=%s)" % str(timeout))
+        readable,writeable,ignore = select.select(readfd,writefd,[],timeout)
+        print("select return")
+
+        active_peers = peers.process_io(readable, writeable)
+
+        #
+        # Protocol processing
+        #
+
+        NEED_INIT = proton.Endpoint.LOCAL_UNINIT
+        NEED_CLOSE = (proton.Endpoint.LOCAL_ACTIVE|proton.Endpoint.REMOTE_CLOSED)
+        ACTIVE = (proton.Endpoint.LOCAL_ACTIVE|proton.Endpoint.REMOTE_ACTIVE)
+
+        while active_peers:
+            peer = active_peers.pop()
+
+            # wait for authentication
+            if peer.sasl:
+                if peer.sasl.state not in (proton.SASL.STATE_PASS,
+                                           proton.SASL.STATE_FAIL):
+                    print("SASL wait.")
+                    continue
+                if peer.sasl.state == proton.SASL.STATE_FAIL:
+                    print("SASL failed for peer %s" % str(peer))
+                    continue
+
+            conn = peer.connection
+            # do state machine processing on conn
+            if conn.state & NEED_INIT:
+                print("Initializing connection")
+                conn.open()
+                ssn = conn.session()
+
+            ssn = conn.session_head(NEED_INIT)
+            while ssn:
+                print("Initializing session")
+                ssn.open()
+                sender = ssn.sender("sender")
+                sender.target.address = "TARGET"
+                if opts.get_replies:
+                    receiver = ssn.receiver("receiver")
+                    receiver.source.address = "SOURCE"
+                ssn = ssn.next(NEED_INIT)
+
+            link = conn.link_head(NEED_INIT)
+            while link:
+                # @todo: initialize terminus addresses???
+                print("Initializing link")
+                link.open()
+                link = link.next(NEED_INIT)
+
+
+            link = conn.link_head(ACTIVE)
+            while link:
+                print("Link is active")
+                if link.is_receiver:
+                    peer.do_recv(link)
+                else:
+                    peer.do_send(link)
+                link = link.next(ACTIVE)
+
+            delivery = conn.work_head
+            while delivery:
+                # do something... smart.
+                print("Got a delivery! tag=%s" % delivery.tag)
+                print("  writable=%s" % delivery.writable)
+                print("  readable=%s" % delivery.readable)
+                print("  updated=%s" % delivery.updated)
+                print("  pending=%s" % delivery.pending)
+                print("  partial=%s" % delivery.partial)
+                print("  local_state=%s" % delivery.local_state)
+                print("  remote_state=%s" % delivery.remote_state)
+                print("  settled=%s" % delivery.settled)
+
+                delivery = delivery.work_next
+
+            link = conn.link_head(NEED_CLOSE)
+            while link:
+                print("Link close")
+                link.close()
+                link = link.next(NEED_CLOSE)
+
+            ssn = conn.session_head(NEED_CLOSE)
+            while ssn:
+                print("session close")
+                ssn.close()
+                ssn = ssn.next(NEED_CLOSE)
+
+            if conn.state == (NEED_CLOSE):
+                print("conn close")
+                conn.close()
+
+
+    return 0
+
+    #         #########################
+
+
+    #         if ((d.sender.link_state() & (PN_LOCAL_ACTIVE | PN_REMOTE_ACTIVE)) == (PN_LOCAL_ACTIVE | PN_REMOTE_ACTIVE)):
+    #             if d.msgs_pending:
+    #                 while pn_link_credit(d.sender) > 0 and d.msgs_pending:
+    #                     delivery = pn_delivery(d.sender, "delivery-%d" %
+    #                                            d.msgs_pending)
+    #                     rc = pn_link_send(d.sender, "message")
+    #                     assert(rc >= 0)
+    #                     d.msgs_pending -= 1
+    #                     pn_link_advance(d.sender)
+    #                     pn_delivery_settle(delivery)
+    #                 if pn_link_credit(d.sender) == 0:
+    #                     if d.msgs_pending > 0:
+    #                         pn_link_offer(d.sender, d.msgs_pending)
+    #             else:
+    #                 d.sender.close()
+    #         elif ((d.sender.link.state() & PN_REMOTE_CLOSED)):
+    #             if (d.sender.link.state() & PN_LOCAL_ACTIVE):
+    #                 d.sender.close()
+
+    #         if ((d.receiver.link_state() & (PN_LOCAL_ACTIVE | PN_REMOTE_ACTIVE)) == (PN_LOCAL_ACTIVE | PN_REMOTE_ACTIVE)):
+
+    #             while pn_link_queued(d.receiver) > 0:
+    #                 delivery = pn_link_current(d.receiver)
+    #                 # read all bytes of message
+    #                 rc, msg = pn_link_recv(receiver.link,
+    #                                        pn_delivery_pending(delivery))
+    #                 assert(rc >= 0)
+    #                 d.replies_received += 1
+    #                 pn_delivery_update(delivery, PN_ACCEPTED)
+    #                 pn_link_advance(d.receiver)
+    #                 pn_delivery_settle(delivery)
+
+    #             if d.replies_received < opts.msg_count:
+    #                 if pn_link_remote_credit(d.receiver) == 0:
+    #                     pn_link_flow(d.receiver, 1)
+    #             else:
+    #                 d.receiver.close()
+    #         elif ((d.receiver.link.state() & PN_REMOTE_CLOSED)):
+    #             if (d.receiver.link.state() & PN_LOCAL_ACTIVE):
+    #                 d.receiver.close()
+
+
+
+    #     # if connection down, mark destination done
+    #     # if msgs remain and credit, send msg
+    #     # if msgs remain and no credit, send offer
+    #     # if no msg remain, send drained
+    #     # once all sent msgs settled, sender done
+    #     # if replies and replies available
+    #     #   recv and settle them
+    #     #   once all recv deliveries settled, recv done
+
+    # # wait until we authenticate with the server
+    # while pn_sasl_state(sender.sasl) not in (PN_SASL_PASS, PN_SASL_FAIL):
+    #     sender.wait()
+    #     if sender.closed():
+    #         sender.log("connection failed")
+    #         return -1;
+
+    # if pn_sasl_state(sender.sasl) == PN_SASL_FAIL:
+    #     print("Error: Authentication failure")
+    #     return -1
+
+
+
+    # sent = 0
+    # while send < opts.msg_count:
+    # pendingSends = list(options.messages)
+    # while pendingSends:
+    #     # wait until the server grants us some send credit
+    #     if pn_link_credit(sender.link) == 0:
+    #         sender.log("wait for credit")
+    #         sender.wait()
+
+    #     while pn_link_credit(sender.link) > 0 and not sender.closed():
+    #         msg = pendingSends.pop(0)
+    #         sender.log("sending %s" % msg)
+    #         d = pn_delivery(sender.link, "post-delivery-%s" % len(pendingSends))
+    #         rc = pn_link_send(sender.link, msg)
+    #         if (rc < 0):
+    #             print("Error: sending message: %s" % rc)
+    #             return -2
+    #         assert rc == len(msg)
+    #         pn_link_advance(sender.link)  # deliver the message
+
+    #     # settle any deliveries that the server has accepted
+    #     sender.settle()
+
+    # def settle(self):
+    #     """ In order to be sure that the remote has accepted the message, we
+    #     need to wait until the message's delivery has been remotely settled.
+    #     Once that occurs, we can release the delivery by settling it.
+    #     """
+    #     d = pn_unsettled_head(self.link)
+    #     while d:
+    #         _next = pn_unsettled_next(d)
+    #         # if the remote has either settled this delivery OR set the
+    #         # disposition, we consider the message received.
+    #         disp = pn_delivery_remote_state(d)
+    #         if disp and disp != PN_ACCEPTED:
+    #             print("Warning: message was not accepted by the remote!")
+    #         if disp or pn_delivery_settled(d):
+    #             pn_delivery_settle(d)
+    #         d = _next
+
+
+    # # done sending, now block until any pending deliveries are settled
+    # sender.log("Done sending messages, waiting for deliveries to settle...");
+    # while pn_link_unsettled(sender.link) > 0 and not sender.closed():
+    #     sender.wait()
+    #     sender.settle()
+
+
+
+    # print "Sending to %s, server=%s exchange=%s namespace=%s fanout=%s" % (
+    #     topic, opts.server, opts.exchange, opts.namespace, str(opts.fanout))
+
+    # # teardown
+    # for conn in conns:
+    #     pn_connection_close(conn)
+    #     # now wait for the connector to close
+    #     while not pn_connector_closed(self.cxtr):
+    #         self.wait()
+
+    # for link in links:
+    #     pn_link_free(link);
+
+    # for ssn in sessions:
+    #     pn_session_free(ssn);
+
+    # for conn in conns:
+    #     pn_connection_free(conn);
+    #     pn_connector_free(cxtr);
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
